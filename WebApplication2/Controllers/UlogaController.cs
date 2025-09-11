d0d8c76bf41e5a2209cef1c43d90641e750973fa
VyšnýAlex <VynAlex@0>
2025-09-11 09:48:45 +0200

    tabel

--- WebApplication2/Controllers/UlogaController.cs
+++ WebApplication2/Controllers/UlogaController.cs
@@ -17,5 +17,24 @@ namespace WebApplication2.Controllers
         {
             return View();
         }
+        public IActionResult Uloha4()
+        {
+            List<Useri> users = new List<Useri>()
+            {
+                new Useri() {
+                    Name = "TesN",
+                    Email = "Test@",
+                    Password = "12345",
+                    Username = "TestUn",
+                },
+                new Useri() {
+                    Name = "TesN1",
+                    Email = "Test@1",
+                    Password = "123451",
+                    Username = "TestUn1",
+                }
+            };
+            return View(users);
+        }
     }
 }
---
+++ WebApplication2/Models/Useri.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Useri 
+    {
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string Username { get; set; } = "";
+    }
+}
