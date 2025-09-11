66a1c058de4b121a736040b5c5665cebb3a741be
VyšnýAlex <VynAlex@0>
2025-09-11 10:12:31 +0200

    finalis

--- WebApplication2/Controllers/UlogaController.cs
+++ WebApplication2/Controllers/UlogaController.cs
@@ -36,5 +36,38 @@ namespace WebApplication2.Controllers
             };
             return View(users);
         }
+        public IActionResult Uloha5()
+        {
+            List<Useri> users = new List<Useri>()
+            {
+                new Useri() {
+                    Name = "TesN",
+                    Email = "Test@",
+                    Password = "12345",
+                    Username = "TestUn",
+                },
+                new Useri() {
+                    Name = "TesN1",
+                    Email = "Test@1",
+                    Password = "123451",
+                    Username = "TestUn1",
+                },
+                new Useri()
+                {
+                    Name="Steph"
+                    ,Email="seph@special.com"
+                    ,Password="1sd4"
+                    ,Username="StepS"
+                }
+                ,new Useri()
+                {
+                    Name="someoneElse"
+                    ,Email="someone@else.com"
+                    ,Password="4"
+                    ,Username="Heeeeeeeee"
+                }
+            };
+            return View(users);
+        }
     }
 }
