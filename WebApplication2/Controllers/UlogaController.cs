b0ee9fd20baa07ea30218e9ad7135705aeeedbcc
VyšnýAlex <VynAlex@0>
2025-09-11 08:13:15 +0200

    4

---
+++ WebApplication2/Controllers/UlogaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class UlogaController : Controller
+    {
+        public IActionResult Uloha()
+        {
+            return View();
+        }
+    }
+}
